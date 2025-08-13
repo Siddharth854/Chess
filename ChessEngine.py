a240cc366a80901c0a5549ddb180ea44abb77bf0
Siddharth Joshi <59554898+Siddharth854@users.noreply.github.com>
2025-08-13 23:58:20 +0530

    Add files via upload

---
+++ ChessEngine.py
@@ -0,0 +1,21 @@
+"""
+responsible dore stiring info of current game state
+determining the valid move at the xcurrent stage
+keeps a move log
+"""
+
+class GameState():
+    def __init__(self):
+        # board is 8*8 2d list each element has 2 characters
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
